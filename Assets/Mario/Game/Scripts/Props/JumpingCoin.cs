1fdd7aedff00b5a5d9dd29164796a61f7df98fb5
Agustin Rosso <AgustinRosso_1@hotmail.com>
2023-05-10 20:58:57 -0300

    Se agrea scrip para destrir moneda que salta de la caja al completar animacion

---
+++ Assets/Mario/Game/Scripts/Props/JumpingCoin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Mario.Game.Props
+{
+    public class JumpingCoin : MonoBehaviour
+    {
+        public void OnJumpCompleted()
+        { 
+            Destroy(gameObject);
+        }
+    }
+}
