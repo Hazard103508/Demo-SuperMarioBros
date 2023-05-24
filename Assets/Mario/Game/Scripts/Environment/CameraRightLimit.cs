46089ba20eccc68cce6ebfc6e65f1c73e59105f7
Agustin Rosso <AgustinRosso_1@hotmail.com>
2023-05-24 14:01:54 -0300

    Se agrega script para limitar desplazamiento de la camara fuera del mapa

---
+++ Assets/Mario/Game/Scripts/Environment/CameraRightLimit.cs
@@ -0,0 +1,27 @@
+using Mario.Application.Services;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Mario.Game.Environment
+{
+    public class CameraRightLimit : MonoBehaviour
+    {
+        private int limitXPosition;
+
+        private void Awake()
+        {
+            limitXPosition = AllServices.GameDataService.CurrentMapProfile.MapsSections.Sum(m => m.Width);
+        }
+        private void LateUpdate()
+        {
+
+            var cam = Camera.main;
+            var topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
+
+            if (topRight.x > limitXPosition)
+                this.transform.position -= Vector3.right * (topRight.x - limitXPosition);
+        }
+    }
+}
