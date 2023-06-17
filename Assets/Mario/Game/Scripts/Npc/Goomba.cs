8179159053c30dcf567fcdcd86f391f46388d896
Agustin Rosso <agustinr@bigfootgaming.net>
2023-06-17 20:58:53 -0300

    Se agrega Npc Goomba con funcionalidad basica

---
+++ Assets/Mario/Game/Scripts/Npc/Goomba.cs
@@ -0,0 +1,78 @@
+using Mario.Application.Services;
+using Mario.Game.ScriptableObjects.Items;
+using System;
+using UnityEngine;
+using UnityShared.Behaviours.Various.RaycastRange;
+using UnityShared.Commons.Structs;
+
+namespace Mario.Game.Npc
+{
+    public class Goomba : MonoBehaviour
+    {
+        [SerializeField] private GoombaProfile _goombaProfile;
+        [SerializeField] private RaycastRange[] _raycastRanges = null;
+
+        private Vector3 _currentSpeed;
+        private Bounds<bool> _proximityBlock = new();
+
+        #region Unity Methods
+        protected virtual void Awake()
+        {
+            _currentSpeed = Vector2.right * _goombaProfile.MoveSpeed;
+            Array.ForEach(_raycastRanges, r => r.SpriteSize = new Size2(0.9f, 1));
+        }
+        private void Update()
+        {
+
+            if (!AllServices.PlayerService.CanMove)
+                return;
+
+            CalculateWalk();
+            CalculateGravity();
+            
+            Move();
+        }
+        #endregion
+
+        #region Private Methods
+        private void CalculateWalk()
+        {
+            if (_proximityBlock.right)
+                _currentSpeed.x = -Mathf.Abs(_currentSpeed.x);
+            else if (_proximityBlock.left)
+                _currentSpeed.x = Mathf.Abs(_currentSpeed.x);
+        }
+        private void CalculateGravity()
+        {
+            _currentSpeed.y -= _goombaProfile.FallSpeed * Time.deltaTime;
+            if (_proximityBlock.bottom)
+            {
+                if (_currentSpeed.y < 0)
+                    _currentSpeed.y = 0;
+            }
+            else
+            {
+                if (_currentSpeed.y < -_goombaProfile.MaxFallSpeed)
+                    _currentSpeed.y = -_goombaProfile.MaxFallSpeed;
+            }
+        }
+        private void Move()
+        {
+            var nextPosition = transform.position + _currentSpeed * Time.deltaTime;
+
+            // ajusto posicion de contacto con el suelo
+            if (_proximityBlock.bottom && _currentSpeed.y == 0)
+                nextPosition.y = Mathf.Round(nextPosition.y);
+
+            transform.position = nextPosition;
+        }
+        #endregion
+
+        #region On Ray Range Hit
+        public void OnProximityRayHitLeft(RayHitInfo hitInfo) => _proximityBlock.left = hitInfo.IsBlock;
+        public void OnProximityRayHitRight(RayHitInfo hitInfo) => _proximityBlock.right = hitInfo.IsBlock;
+        public void OnProximityRayHitBottom(RayHitInfo hitInfo) => _proximityBlock.bottom = hitInfo.IsBlock;
+        public void OnProximityRayHitTop(RayHitInfo hitInfo) => _proximityBlock.top = hitInfo.IsBlock;
+        #endregion
+    }
+}
---
+++ Assets/Mario/Game/Scripts/ScriptableObjects/Npc/GoombaProfile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Mario.Game.ScriptableObjects.Items
+{
+    [CreateAssetMenu(fileName = "GoombaProfile", menuName = "ScriptableObjects/Game/Npc/GoombaProfile", order = 0)]
+    public class GoombaProfile : ScriptableObject
+    {
+        public LayerMask GroundLayer;
+        public float MoveSpeed;
+        public float FallSpeed;
+        public float MaxFallSpeed;
+    }
+}
