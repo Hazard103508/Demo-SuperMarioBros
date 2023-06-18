b5de43c49a036773545246cc2ed04c000805ff81
Agustin Rosso <agustinr@bigfootgaming.net>
2023-06-17 21:20:43 -0300

    Se agrega colicion basica de npc con player

--- Assets/Mario/Game/Scripts/Npc/Goomba.cs
+++ Assets/Mario/Game/Scripts/Npc/Goomba.cs
@@ -1,4 +1,6 @@
 using Mario.Application.Services;
+using Mario.Game.Interfaces;
+using Mario.Game.Player;
 using Mario.Game.ScriptableObjects.Items;
 using System;
 using UnityEngine;
@@ -7,29 +9,29 @@ using UnityShared.Commons.Structs;
 
 namespace Mario.Game.Npc
 {
-    public class Goomba : MonoBehaviour
+    public class Goomba : MonoBehaviour, ITopHitable, IBottomHitable, ILeftHitable, IRightHitable
     {
         [SerializeField] private GoombaProfile _goombaProfile;
+        [SerializeField] private Animator _animator;
         [SerializeField] private RaycastRange[] _raycastRanges = null;
 
         private Vector3 _currentSpeed;
         private Bounds<bool> _proximityBlock = new();
 
         #region Unity Methods
-        protected virtual void Awake()
+        private void Awake()
         {
             _currentSpeed = Vector2.right * _goombaProfile.MoveSpeed;
             Array.ForEach(_raycastRanges, r => r.SpriteSize = new Size2(0.9f, 1));
         }
         private void Update()
         {
-
             if (!AllServices.PlayerService.CanMove)
                 return;
 
             CalculateWalk();
             CalculateGravity();
-            
+
             Move();
         }
         #endregion
@@ -66,6 +68,19 @@ namespace Mario.Game.Npc
 
             transform.position = nextPosition;
         }
+        private void KillGoomba()
+        {
+
+        }
+        private void DamagePlayer(PlayerController player)
+        {
+            if (!enabled)
+                return;
+
+            _animator.speed = 0;
+            enabled = false;
+            player.Kill();
+        }
         #endregion
 
         #region On Ray Range Hit
@@ -73,6 +88,13 @@ namespace Mario.Game.Npc
         public void OnProximityRayHitRight(RayHitInfo hitInfo) => _proximityBlock.right = hitInfo.IsBlock;
         public void OnProximityRayHitBottom(RayHitInfo hitInfo) => _proximityBlock.bottom = hitInfo.IsBlock;
         public void OnProximityRayHitTop(RayHitInfo hitInfo) => _proximityBlock.top = hitInfo.IsBlock;
+        #endregion
+
+        #region On Player Hit
+        public void OnHitFromTop(PlayerController player) => KillGoomba();
+        public void OnHitFromBottom(PlayerController player) => DamagePlayer(player);
+        public void OnHitFromLeft(PlayerController player) => DamagePlayer(player);
+        public void OnHitFromRight(PlayerController player) => DamagePlayer(player);
         #endregion
     }
 }
