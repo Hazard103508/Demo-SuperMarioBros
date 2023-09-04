53c12fc722c189d1966bb0fba8d6cb3bc70103f6
Agustin Rosso <agustinr@bigfootgaming.net>
2023-09-03 21:13:03 -0300

    player state machine

--- Assets/Mario/Game/Scripts/Player/Modes/PlayerModeSuper.cs
+++ Assets/Mario/Game/Scripts/Player/Modes/PlayerModeSuper.cs
@@ -6,15 +6,15 @@ namespace Mario.Game.Player
         {
             base.ModeProfile = player.Profile.Modes.Super;
             base.StateIdle = new PlayerStateSuperIdle(player);
-            //base.StateRun = new PlayerStateBigRun(player);
-            //base.StateStop = new PlayerStateStop(player);
-            //base.StateJump = new PlayerStateJump(player);
-            //base.StateFall = new PlayerStateFall(player);
-            //base.StateNerf = new PlayerStateBigNerf(player);
-            //base.StateDeath = new PlayerStateDeath(player);
-            //base.StateFlag = new PlayerStateFlag(player);
-            //base.StateDucking = new PlayerStateDucking(player);
-            //base.StateDuckingJump = new PlayerStateDuckingJump(player);
+            base.StateRun = new PlayerStateSuperRun(player);
+            base.StateStop = new PlayerStateStop(player);
+            base.StateJump = new PlayerStateJump(player);
+            base.StateFall = new PlayerStateFall(player);
+            base.StateNerf = new PlayerStateBigNerf(player); // big nerf is OK
+            base.StateDeath = new PlayerStateDeath(player);
+            base.StateFlag = new PlayerStateFlag(player);
+            base.StateDucking = new PlayerStateDucking(player);
+            base.StateDuckingJump = new PlayerStateDuckingJump(player);
         }
     }
 }
