089ed3a0b50f16c381b8683df4d105d4521b02f5
Agustin Rosso <AgustinRosso_1@hotmail.com>
2023-10-28 18:24:35 +0200

    mejoras en SO de NPC

--- Assets/Mario/Game/Scripts/ScriptableObjects/Npc/GoombaProfile.cs
+++ Assets/Mario/Game/Scripts/ScriptableObjects/Npc/GoombaProfile.cs
@@ -5,11 +5,16 @@ namespace Mario.Game.ScriptableObjects.Items
     [CreateAssetMenu(fileName = "GoombaProfile", menuName = "ScriptableObjects/Game/Npc/GoombaProfile", order = 0)]
     public class GoombaProfile : ScriptableObject
     {
-        public LayerMask GroundLayer;
-        public float MoveSpeed;
-        public float FallSpeed;
-        public float MaxFallSpeed;
-        public float JumpAcceleration;
-        public int Points;
+        [SerializeField]private float _moveSpeed;
+        [SerializeField]private float _fallSpeed;
+        [SerializeField]private float _maxFallSpeed;
+        [SerializeField]private float _jumpAcceleration;
+        [SerializeField] private int _points;
+
+        public float MoveSpeed => _moveSpeed;
+        public float FallSpeed => _fallSpeed;
+        public float MaxFallSpeed => _maxFallSpeed;
+        public float JumpAcceleration => _jumpAcceleration;
+        public int Points => _points;
     }
 }
--- Assets/Mario/Game/Scripts/ScriptableObjects/Npc/KoopaProfile.cs
+++ Assets/Mario/Game/Scripts/ScriptableObjects/Npc/KoopaProfile.cs
@@ -5,15 +5,23 @@ namespace Mario.Game.ScriptableObjects.Items
     [CreateAssetMenu(fileName = "KoopaProfile", menuName = "ScriptableObjects/Game/Npc/KoopaProfile", order = 1)]
     public class KoopaProfile : ScriptableObject
     {
-        public LayerMask GroundLayer;
-        public float MoveSpeed;
-        public float BouncingSpeed;
-        public float FallSpeed;
-        public float MaxFallSpeed;
-        public float JumpAcceleration;
-        public int PointsHit1;
-        public int PointsHit2;
-        public int PointsKill;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _bouncingSpeed;
+        [SerializeField] private float _fallSpeed;
+        [SerializeField] private float _maxFallSpeed;
+        [SerializeField] private float _jumpAcceleration;
+        [SerializeField] private int _pointsHit1;
+        [SerializeField] private int _pointsHit2;
+        [SerializeField] private int _pointsKill;
+
+        public float MoveSpeed => _moveSpeed;
+        public float BouncingSpeed => _bouncingSpeed;
+        public float FallSpeed => _fallSpeed;
+        public float MaxFallSpeed => _maxFallSpeed;
+        public float JumpAcceleration => _jumpAcceleration;
+        public int PointsHit1 => _pointsHit1;
+        public int PointsHit2 => _pointsHit2;
+        public int PointsKill => _pointsKill;
 
     }
 }
