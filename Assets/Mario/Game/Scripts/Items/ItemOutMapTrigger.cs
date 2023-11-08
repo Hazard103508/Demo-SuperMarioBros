465a743b8d122d8817973b0bf390b8c5ebc9daaf
Agustin Rosso <AgustinRosso_1@hotmail.com>
2023-11-08 12:41:20 +0100

    correcciones en muerte del personaje

--- Assets/Mario/Game/Scripts/Items/ItemOutMapTrigger.cs
+++ Assets/Mario/Game/Scripts/Items/ItemOutMapTrigger.cs
@@ -7,31 +7,40 @@ namespace Mario.Game.Items
     public class ItemOutMapTrigger : MonoBehaviour
     {
         #region Objects
+        [SerializeField] private Vector2 _margin;
         private float bottomBorder;
-        private SpriteRenderer spriteRenderer;
+
+        private bool isOut;
         #endregion
 
         #region Events
-        public UnityEvent onOut;
+        public UnityEvent onOutFromDown;
+        public UnityEvent onOutFromLeft;
+        public UnityEvent onOutFromRight;
         #endregion
 
         #region Unity Methods
         void Awake()
         {
-            spriteRenderer = GetComponent<SpriteRenderer>();
             var cam = Camera.main;
             var downLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
-
-            var scale = transform.localScale;
-            var spritePivot = 1 - spriteRenderer.sprite.pivot.y / spriteRenderer.sprite.rect.height;
-            bottomBorder = downLeft.y - (spriteRenderer.sprite.bounds.size.y * scale.y) * spritePivot;
+            bottomBorder = downLeft.y - _margin.y;
         }
-
+        private void OnEnable()
+        {
+            isOut = false;
+        }
         void LateUpdate()
         {
+            if (isOut)
+                return;
+
             var pos = transform.position;
             if (pos.y < bottomBorder)
-                onOut.Invoke();
+            {
+                onOutFromDown.Invoke();
+                isOut = true;
+            }
         }
         #endregion
     }
