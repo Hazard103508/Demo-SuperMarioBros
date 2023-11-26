0ed79b70dca869719e2b9cd2d7322bcbcfcc66ee
Agustin Rosso <AgustinRosso_1@hotmail.com>
2023-11-26 18:31:55 +0100

    correccion en detection de sprite fuera de pantalla

--- Assets/Mario/Game/Scripts/Items/ItemOutMapTrigger.cs
+++ Assets/Mario/Game/Scripts/Items/ItemOutMapTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityShared.Commons.Structs;
@@ -8,71 +9,80 @@ namespace Mario.Game.Items
     public class ItemOutMapTrigger : MonoBehaviour
     {
         #region Objects
-        [SerializeField] private Vector2 _margin;
-        [SerializeField] private bool _isSingleUse = true;
-
-        private Bounds<float> bordersOut;
-        private bool isOut;
-        #endregion
-
-        #region Events
-        public UnityEvent onOutFromDown;
-        public UnityEvent onOutFromTop;
-        public UnityEvent onOutFromLeft;
-        public UnityEvent onOutFromRight;
+        [SerializeField] private Bounds<ScreenEdge> borders;
         #endregion
 
         #region Unity Methods
-        private void OnEnable()
-        {
-            isOut = false;
-        }
         void LateUpdate()
         {
-            if (_isSingleUse && isOut)
-                return;
-
             var cam = Camera.main;
             var downLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
             var topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, cam.nearClipPlane));
 
-            bordersOut = new Bounds<float>()
-            {
-                left = downLeft.x - _margin.x,
-                right = topRight.x + _margin.x,
-                bottom = downLeft.y - _margin.y,
-                top = topRight.y + _margin.y,
-            };
-
-            var pos = transform.position;
-
-            if (pos.y < bordersOut.bottom)
-            {
-                onOutFromDown.Invoke();
-                isOut = true;
+            if (ChechBottomEdge(downLeft.y))
                 return;
-            }
 
-            if (pos.y > bordersOut.top)
-            {
-                onOutFromTop.Invoke();
-                isOut = true;
+            if (ChechTopEdge(topRight.y))
                 return;
-            }
 
-            if (pos.x < bordersOut.left)
-            {
-                onOutFromLeft.Invoke();
-                isOut = true;
+            if (ChechLeftEdge(downLeft.x))
                 return;
-            }
 
-            if (pos.x > bordersOut.right)
-            {
-                onOutFromRight.Invoke();
-                isOut = true;
+            if (ChechRightEdge(topRight.x))
                 return;
+        }
+        #endregion
+
+        #region Private Methods
+        private bool ChechBottomEdge(float screenEdge)
+        {
+            var border = borders.bottom;
+            if (border.Check && transform.position.y < screenEdge - border.Margin)
+            {
+                border.onOut.Invoke();
+                return true;
             }
+            return false;
+        }
+        private bool ChechTopEdge(float edge)
+        {
+            var border = borders.top;
+            if (border.Check && transform.position.y > edge + border.Margin)
+            {
+                border.onOut.Invoke();
+                return true;
+            }
+            return false;
+        }
+        private bool ChechLeftEdge(float edge)
+        {
+            var border = borders.left;
+            if ( border.Check && transform.position.x < edge - border.Margin)
+            {
+                border.onOut.Invoke();
+                return true;
+            }
+            return false;
+        }
+        private bool ChechRightEdge(float edge)
+        {
+            var border = borders.right;
+            if (border.Check && transform.position.x > edge + border.Margin)
+            {
+                border.onOut.Invoke();
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Structures
+        [Serializable]
+        public class ScreenEdge
+        {
+            public bool Check;
+            public float Margin;
+            public UnityEvent onOut;
         }
         #endregion
     }
